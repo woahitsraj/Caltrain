7548ee07039b57a77bd33e060339045bcde367a2
Rajan Singh <rajan1992@hotmail.com>
2016-08-06 21:06:55 -0400

    Merged stop and main controllers
    Also got NB and SB timetables showing

--- client/dev/transit/controllers/stopController.js
+++ client/dev/transit/controllers/stopController.js
@@ -4,21 +4,6 @@
 	ng.module('public-transport')
 		.controller('stopController', ['stopService', '$scope', '$http',
 			function(stopService, $scope) {
-				$scope.stops = [];
-				stopService.getAll().success(function(data) {
-					$.each(data, function(i, el) {
-						var flag = true;
-						$.each($scope.stops, function(index, val) {
-							if (val.stop_name == el.stop_name) {
-								flag = false;
-							}
-						});
-						if (flag) {
-							$scope.stops.push(el);
-						}
-					});
-
-				});
 
 			}
 		]);
