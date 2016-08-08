e09e64a682c6fb61c1eefb300cb7d6a0492d43e6
Rajan Singh <rajan1992@hotmail.com>
2016-08-08 12:00:57 -0400

    removed old todo resources

--- tasks/client/sw_precach.js
+++ tasks/client/sw_precach.js
@@ -1,12 +1,21 @@
 import gulp from 'gulp';
 
 gulp.task('generate-service-worker', function(callback) {
-  var path = require('path');
-  var swPrecache = require('sw-precache');
-  var rootDir = 'client/dev';
+	var path = require('path');
+	var swPrecache = require('sw-precache');
+	var rootDir = 'client/dev';
 
-  swPrecache.write(path.join(rootDir, 'service-worker.js'), {
-    staticFileGlobs: [rootDir + '/**/*.{js,html,css,png,jpg,gif,svg,eot,ttf,woff}'],
-    stripPrefix: rootDir
-  }, callback);
+	swPrecache.write(path.join(rootDir, 'service-worker.js'), {
+		staticFileGlobs: [rootDir + '/**/*.{js,html,css,png,jpg,gif,svg,eot,ttf,woff}'],
+		stripPrefix: rootDir,
+		runtimeCaching: [{
+			urlPattern: /api/,
+			handler: 'cacheFirst',
+			options: {
+				cache: {
+					name: 'stop-cache'
+				}
+			}
+		}]
+	}, callback);
 });
