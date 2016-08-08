5f71e1e0abee1a08afdca9e1c40482702b68b665
Rajan Singh <rajan1992@hotmail.com>
2016-08-08 11:34:40 -0400

    service worker is now caching and serving app offline

--- tasks/client/sw_precach.js
+++ tasks/client/sw_precach.js
@@ -3,7 +3,7 @@ import gulp from 'gulp';
 gulp.task('generate-service-worker', function(callback) {
   var path = require('path');
   var swPrecache = require('sw-precache');
-  var rootDir = 'client';
+  var rootDir = 'client/dev';
 
   swPrecache.write(path.join(rootDir, 'service-worker.js'), {
     staticFileGlobs: [rootDir + '/**/*.{js,html,css,png,jpg,gif,svg,eot,ttf,woff}'],
